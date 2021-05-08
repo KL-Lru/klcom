3db5322d18ac96e0ba350bce9564faab39e199da
KL-Lru <kilattoeruru@outlook.jp>
2021-05-09 02:30:11 +0900

    schema, connection修正

--- src/repositories/connection.rs
+++ src/repositories/connection.rs
@@ -7,11 +7,11 @@ use std::env;
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 pub fn build_db_pool() -> DbPool {
-    dotenv().ok();
+  dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let manager = ConnectionManager::<PgConnection>::new(database_url);
-    r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create pool.")
+  let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+  let manager = ConnectionManager::<PgConnection>::new(database_url);
+  r2d2::Pool::builder()
+    .build(manager)
+    .expect("Failed to create pool.")
 }
---
+++ src/repositories/schema.rs
@@ -0,0 +1,50 @@
+table! {
+    post_tags (id) {
+        id -> Int4,
+        post -> Int4,
+        tag -> Varchar,
+    }
+}
+
+table! {
+    posts (id) {
+        id -> Int4,
+        author -> Int4,
+        title -> Varchar,
+        body -> Text,
+        publish -> Bool,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
+    }
+}
+
+table! {
+    tags (tag_name) {
+        id -> Int4,
+        tag_name -> Varchar,
+    }
+}
+
+table! {
+    user_roles (role_name) {
+        id -> Int4,
+        role_name -> Varchar,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Int4,
+        name -> Varchar,
+        email -> Varchar,
+        password -> Varchar,
+        role -> Varchar,
+    }
+}
+
+joinable!(post_tags -> posts (post));
+joinable!(post_tags -> tags (tag));
+joinable!(posts -> users (author));
+joinable!(users -> user_roles (role));
+
+allow_tables_to_appear_in_same_query!(post_tags, posts, tags, user_roles, users,);
